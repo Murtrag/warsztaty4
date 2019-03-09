4de404d48f9d29b3b49cb12f9b7cb88b44bcf0df
Krzysztof Trutkowski <trutkowski@gmail.com>
2019-03-09 14:28:40 +0100

    Add room view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from main.models import *
 # Create your views here.
+
+
+def add_room(request):
+    if request.method == "GET":
+        return render(request, "add_room.html")
+    if request.method == "POST":
+        rooms = Room()
+        rooms.name = request.POST['name']
+        rooms.capacity = request.POST['capacity']
+        rooms.tv = request.POST['tv']
+        rooms.air_conditioning = request.POST['air_conditioning']
+        rooms.save()
+        return HttpResponse("Dodano pokój")
+
