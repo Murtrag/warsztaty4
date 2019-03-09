c3b1735c2767088ed96ff364c9fb909b1a153a84
Murtrag <lukasz.czernik@gmail.com>
2019-03-09 15:07:06 +0100

    fix

--- orderer_2000/urls.py
+++ orderer_2000/urls.py
@@ -5,12 +5,8 @@ from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     url('^all_rooms/', views.RoomList.as_view()),
-    url(r'^add_room/$', views.add_room)
-=======
-    url('^test/', views.test),
     url(r'^add_room/$', views.add_room),
     url(r'^room/delete/(?P<id>\d+)$', views.delete_room)
->>>>>>> d852b55f1293a867242a9aea777bef66c38bd32d
+
 ]
