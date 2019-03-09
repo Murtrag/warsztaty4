d852b55f1293a867242a9aea777bef66c38bd32d
Krzysztof Trutkowski <trutkowski@gmail.com>
2019-03-09 14:59:08 +0100

    url for add_room changed

--- orderer_2000/urls.py
+++ orderer_2000/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url('^test/', views.test),
     url(r'^add_room/$', views.add_room),
-    url(r'^delete_room/(?P<id>\d+)$', views.delete_room)
+    url(r'^room/delete/(?P<id>\d+)$', views.delete_room)
 ]
