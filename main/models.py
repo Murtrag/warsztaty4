ceeac0425536a32967c051dbc8bc22e3d434bcc2
Krzysztof Trutkowski <trutkowski@gmail.com>
2019-03-09 13:55:55 +0100

    Room and Reservation models

--- main/models.py
+++ main/models.py
@@ -1,4 +1,20 @@
 from django.db import models
 
 # Create your models here.
-# Łukasz jest GUPI
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=64)
+    capacity = models.IntegerField()
+    projector = models.BooleanField(default=False)
+    tv = models.BooleanField(default=False)
+    air_conditioning = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation(models.Model):
+    date = models.DateField()
+    rooms = models.ForeignKey(Room, on_delete=models.CASCADE)
+    comment = models.TextField()
